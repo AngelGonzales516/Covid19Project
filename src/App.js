bfb82e1a91311952101f8648873da0532b38253c
Angel Gonzales <angel@MSI>
2021-03-19 16:14:52 -0600

    creacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import BrandHeader from './components/BrandHeader/BrandHeader';
+import Covid from './components/Covid/Covid';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <section>
+        <BrandHeader></BrandHeader>
+        <Switch>
+          <Route path="/" exact>
+              <Covid />
+          </Route>
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
